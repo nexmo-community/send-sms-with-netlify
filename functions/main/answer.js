94aff654d23478cf80e6dad549eb655783736fff
Garann Means <garann.means@vonage.com>
2020-06-25 16:06:51 +0100

    Initial commit

---
+++ functions/main/answer.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
+  applicationId: process.env.APP_ID,
+  privateKey: './private.key'
+});
+
+exports.handler = async (event, context) => {
+  try {
+    nexmo.channel.send(
+      {
+        "type": 'sms',
+        "number": JSON.parse(event.body.from.number)
+      },
+      {
+        "type": 'sms',
+        "number": '447418344242'
+      },
+      {
+        content: {
+          "type": 'text',
+          "text": 'Hello, here is your SMS!'
+        }
+      },
+      (e, data) => {
+        if (e) {
+          console.error(e);
+          return;
+        }
+        console.log(data);
+      }
+    );
+
+    return {
+      statusCode: 204
+    }
+  } catch(e) {
+    return { statusCode: 500, body: 'Error: ' + e };
+  }
+};
